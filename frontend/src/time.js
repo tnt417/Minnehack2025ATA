168240b28096ad280fb97a24092ea2ca4d8ebe9e
Whatshisname <120993368+Whatshisname303@users.noreply.github.com>
2025-02-09 09:23:20 -0600

    Add time utils

---
+++ frontend/src/time.js
@@ -0,0 +1,27 @@
+class WeekManager {
+    constructor(startTime, endTime) {
+        const time = Date.now();
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    // returns submission, voting, or results string literals
+    getWeekProgress() {
+        const delta = Date.now() - this.startTime;
+        const weekDuration = this.endTime - this.startTime;
+        const progress = delta / weekDuration;
+
+        if (progress <= 4/7) {
+            return "submission";
+        } else if (progress <= 5/7) {
+            return "voting";
+        } else {
+            return "results";
+        }
+    }
+
+    static weekAheadFrom(startTime) {
+        const endTime = startTime + 604800000;
+        return endTime;
+    }
+}
