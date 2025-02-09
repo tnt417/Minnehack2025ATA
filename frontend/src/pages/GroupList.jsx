0731685cb16821271d4e5087ea6c2472f7c49392
Tony Rutherford <ar56058@gmail.com>
2025-02-09 00:46:25 -0600

    Connect grouplist to backend

--- frontend/src/pages/GroupList.jsx
+++ frontend/src/pages/GroupList.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import GroupEntry from './components/GroupEntry';
 import backend from '../backend'
 
-const GroupList = ({myGroups}) => {
-  const groups = [
-    { groupName: 'Steve\'s Group', numPeople: 5 },
-    { groupName: 'CSE Majors', numPeople: 3 },
-    { groupName: 'Minneapolis Maniacs', numPeople: 7 },
-  ];
+const GroupList = ({myGroups, authToken}) => {
+  const [groups, setGroups] = useState([
+    // id: memberCount: name:
+  ]);
 
   async function fetchGroups () {
-    var res = backend.get("/")
+    if(myGroups){
+      var res = await backend.get(`/my-groups?auth=${authToken}`)
+      setGroups(res.data)
+      console.log("My groups: ", res)
+    }
+    else
+    {
+      var res = await backend.get(`/all-groups?auth=${authToken}`)
+      setGroups(res.data)
+      console.log("All groups: ", res)
+    }
   }
+
+  useEffect(() => {
+    fetchGroups()
+  }, [myGroups, authToken])
 
   return (
     <div className="space-y-4 w-full m-auto justify-center pt-[50px] bg-gray-100 min-h-screen">
     <h1 className="text-center text-2xl mt-[50px]">{myGroups ? "Your Groups" : "Explore groups in your community..."}</h1>
     {groups.map((group, index) => (
-        <GroupEntry key={index} groupName={group.groupName} joined={myGroups} numPeople={group.numPeople} />
+        <GroupEntry key={index} groupName={group.name} joined={myGroups} numPeople={group.memberCount} />
     ))}
     </div>
   );
